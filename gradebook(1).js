7a051d5d9c813d5abbbc006f0008b71b1b94a0f5
jwang-eng <jwang19@my.waketech.edu>
2025-07-14 12:07:28 -0400

    Create gradebook(1).js

---
+++ gradebook(1).js
@@ -0,0 +1,17 @@
+// TODD: Fetch data from the PostgreSQL database (to be implemented later)
+function fetchGradeData( ) {
+   // This function will query the postgreSQL database and return grade data
+   console.log("Fetching grade data...");
+}
+
+// TODD: Populate the table with grade data
+function populateGradebook (data) {
+       // This function will take the fetched grade data and populate the table
+       console.log("Populating gradebook with data:", data);
+}
+// TODO REMOVE THIS
+// Call the stubs to demostrate the workflow
+const gradeData = fetchGradeData( );
+populateGradebook(gradeData);
+// END REMOVE
+
